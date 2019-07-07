94407e53ac98663843501c1bfc9c8a15cadf913e
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-07 16:25:55 +0200

    Add files via upload

---
+++ Needs.sh
@@ -0,0 +1,28 @@
+clear
+echo "########################"
+echo ""
+echo "     RUNNING      "
+echo ""
+echo "########################"
+echo ""
+echo " Updating && Upgrading termux "
+echo ""
+pkg update && pkg upgrade -y 
+echo "" 
+echo "%%%%%%%%%%%%%%%%%%"
+echo ""
+echo " Installing Requirements "
+echo ""
+echo "%%%%%%%%%%%%%%%%%%"
+pkg install python toilet git ruby -y
+echo "" 
+echo "&&&&&&&&&&&&&&&&&&&&&&&"
+echo ""
+toilet "     Installing Lolcat    "
+echo "" 
+echo "&&&&&&&&&&&&&&&&&&&&&&&"
+gem install lolcat 
+echo ""
+clear 
+toilet -f big " DONE....!" -F gay | lolcat 
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# MasterMarket
-A Simple Market To Install best 10 Hacking Script 
+Script Made by MASTERX
+ 
+Youtube Channel MASTER X
+
+Version 1.0 
+
+Don't Copy or modify the script Without my Permission
+
+NOTE ::: SCRIPT MADE IN PYTHON3 
