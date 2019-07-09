2e2c8f4b3d90232352339cce509e30a098d4647d
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-09 16:20:59 +0200

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,7 @@
+def hodo () : 
+    import os 
+    os.system ( " clear " ) 
+    import os
+    os.system ("cowthink -f ghostbusters 'WELCOME TO MASTERMARKET' | lolcat " ) 
+
+hodo ()
---
+++ toolsmenu.py
@@ -0,0 +1,135 @@
+def so () :
+    import os
+    os.system ("sleep 1 ")
+    import os     
+    os.system ( " toilet -f big 'MasterMarket' -F gay | lolcat ")
+    import os
+    os.system ("   echo ''  ")
+    import os 
+    os.system (" echo ' v 1.1 ' ")
+    import os
+    os.system ( "  echo '' ")
+    import os
+    os.system ( "  echo '' ")
+    import os
+    os.system ( "  echo '' ")
+    import os
+    os.system ( "  echo '' ")
+    name= ("1) Tools ")
+    name1=("2) About ")
+    name2=("3) Update ")
+    print (name)
+    import os
+    os.system ( "  echo '' ")
+    print (name1)
+    import os
+    os.system ( "  echo '' ")
+    print (name2)
+    import os
+    os.system ( "  echo '' ")
+    loop = input (">>>>  ")
+    if loop == '1' :
+       print ("1)EasY_HaCk")
+       print ("2)Metasploit")
+       print ("3)Alpine")
+       print ("4)DarkFly")
+       print ("5)SpammerGrab")
+       print ("6)SayCheese")
+       print ("7)IP-FY")
+       print ("8)ShellFish")
+       print ("9)OSIF")
+       print ("10)BeeF")
+       print ("11)Tool-X")
+       print ("12)Lazymux")
+       print ("13)TxTool")
+       print ("14)HiddenEye")
+       print ("15)MyServer")
+       print ("16)Hammer")
+       print ("17)Sqlmap")
+       print ("18)Santet-Online")
+       print ("19)ipcs")
+       print ("20)KaliLinux")
+    elif loop == '2' :
+         print ("Creator : MASTERX ")
+         print ("Channel Youtube : MASTER X ")
+         print ("Script Version : 1.1 ")
+         print ("My Github Repo : https://github.com/seifsherifssss")
+    else :
+      loop3 = input ("Are You Sure y/n >>>   ")
+      if loop3 == 'y' :
+          import os 
+          os.system (" clear && toilet -f big 'UPDATING' | lolcat && cd /$HOME && rm -rif MasterMarket &&git clone https://github.com/seifsherifssss/MasterMarket ")
+      if loop3 == 'n' :
+         import os
+         os.system("clear")
+         so()
+
+    loop2 = input (">>>>  ")
+
+    if loop2 == '1' :
+      import os
+      os.system("clear && toilet -f big 'EasY_HaCk' | lolcat && cd /$HOME && git clone https://github.com/sabri-zaki/EasY_HaCk")
+    if loop2 == '2' :
+       import os
+       os.system ("clear && toilet -f big 'Metasploit' | lolcat && pkg install unstable-repo && pkg install metasploit ")
+    if loop2 == '3' :
+       import os
+       os.system ("clear && toilet -f big 'Alpine' | lolcat && cd /$HOME && git clone https://github.com/Hax4us/TermuxAlpine")
+    if loop2 == '4' :
+       import os
+       os.system ("clear && toilet -f big 'DarkFly' | lolcat && cd /$HOME &&  git clone https://github.com/Ranginang67/DarkFly-Tool && cd DarkFly-Tool && python2 install.py ")
+    if loop2 == '5' :
+       import os
+       os.system ("clear && toilet -f big 'SpammerGrab' | lolcat && cd /$HOME && git clone https://github.com/Noxturnix/Spammer-Grab && cd Spammer-Grab && ./auto-install.sh")
+    if loop2 == '6' :
+       import os 
+       os.system ("clear && toilet -f big 'SayCheese' | lolcat && cd /$HOME && pkg install git wget php curl openssh -y && git clone https://github.com/thelinuxchoice/saycheese.git")
+    if loop2 == '7' :
+       import os 
+       os.system ("clear && toilet -f big 'IP-FY' | lolcat && cd /$HOME && git clone https://github.com/T4P4N/IP-FY.git")
+    if loop2 == '8' :
+       import os 
+       os.system ("clear && toilet -f big 'Shellphish' | lolcat && cd /$HOME && apt install git wget php openssh -y && git clone https://github.com/thelinuxchoice/shellphish")
+    if loop2 == '9' :
+       import os
+       os.system ("clear && toilet -f big 'OSIF' | lolcat && cd /$HOME && git clone https://github.com/ciku370/OSIF")
+    if loop2 == '10' :
+       import os 
+       os.system (" clear && toilet -f big 'BeeF' | lolcat && termux-open-url https://www.mediafire.com/file/z3kqjbl6j52zmmd/beef-xss-0.4.7.0.tar.gz")
+    if loop2 == '11' :
+      import os
+      os.system("clear && toilet -f big 'Tool-X' | lolcat && cd /$HOME && git clone https://github.com/eroz78/toolx ")
+    if loop2 == '12' :
+       import os
+       os.system ("clear && toilet -f big 'Lazymux' | lolcat && cd /$HOME && git clone https://github.com/Gameye98/Lazymux ")
+    if loop2 == '13' :
+       import os
+       os.system ("clear && toilet -f big 'TxTool' | lolcat && cd /$HOME && git clone https://github.com/kuburan/txtool.git ")
+    if loop2 == '14' :
+       import os
+       os.system ("clear && toilet -f big 'HiddenEye' | lolcat && cd /$HOME &&  git clone -b Termux-Support-Branch https://github.com/DarkSecDevelopers/HiddenEye.git")
+    if loop2 == '15' :
+       import os
+       os.system ("clear && toilet -f big 'MyServer' | lolcat && cd /$HOME && git clone https://github.com/Rajkumrdusad/MyServer.git ")
+    if loop2 == '16' :
+       import os
+       os.system ("clear && toilet -f big 'Hammer' | lolcat && cd /$HOME &&  git clone https://github.com/cyweb/hammer.git ")
+    if loop2 == '17' :
+       import os
+       os.system ("clear && toilet -f big 'SQLmap' | lolcat && cd /$HOME && git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ")
+    if loop2 == '18' :
+       import os
+       os.system ("clear && toilet -f big 'Santet-Online' | lolcat && cd /$HOME && git clone https://github.com/Gameye98/santet-online ")
+    if loop2 == '19' :
+       import os
+       os.system ("clear && toilet -f big 'IPCS' | lolcat && cd /$HOME && git clone https://github.com/kancotdiq/ipcs.git ")
+    if loop2 == '20' :
+       import os
+       os.system (" clear && toilet -f big 'KaliLinux' | lolcat && cd /$HOME && clear && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh && touch /$PREFIX/bin/kali && echo ' bash /$HOME/start-kali.sh' > /$PREFIX/bin/kali && toilet -f big Start Kali Linux by typing kali | lolcat ")
+    
+    else :
+        import os 
+        os.system ("clear")
+        so()
+
+so()
