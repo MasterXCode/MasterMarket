e7d7a9e6bf3377e3cf3036c8e143caa365d8d59a
MASTERX <50529518+MasterXCode@users.noreply.github.com>
2019-12-07 03:04:22 +0200

    Update

--- toolsmenu.py
+++ toolsmenu.py
@@ -95,7 +95,7 @@ def so () :
        os.system ("clear && toilet -f big 'OSIF' | lolcat && cd /$HOME && git clone https://github.com/ciku370/OSIF")
     if loop2 == '10' :
        import os 
-       os.system (" clear && toilet -f big 'BeeF' | lolcat && cd /$HOME && git clone https://github.com/seifsherifssss/BeeF && cd BeeF && bash beef.sh && bash FixBigdecimal.sh ")
+       os.system (" clear && toilet -f big 'BeeF' | lolcat && cd /$HOME && git clone https://github.com/MasterXCode/BeeF && cd BeeF && bash beef.sh && bash FixBigdecimal.sh ")
     if loop2 == '11' :
       import os
       os.system("clear && toilet -f big 'Tool-X' | lolcat && cd /$HOME && git clone https://github.com/eroz78/toolx ")
